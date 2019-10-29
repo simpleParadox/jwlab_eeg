f453c3604181c0f1cfc4e0a6532fbf83b59dfd2a
karlslakov <karlslakov@gmail.com>
2019-10-29 12:44:41 -0700

    cleaning debug and load to python

--- data_cleaning/all_cleaning.m
+++ data_cleaning/all_cleaning.m
@@ -1,5 +1,6 @@
 % set up the script parameters
-FILEPATH = '../data/';
+FILEPATH = '../../../data/';
+FILEPATH_OUT = '../../../data/ml_ready/';
 SUBJECTS = {'999'};
 
 ML_EVENTS = { 'Wait' };
@@ -9,7 +10,8 @@ BOUNDARY_EVENTS = { 'boundary' };
 SAVE_INTERMEDIATE = true;
 
 % we'll gather all cleaned data into this array
-subject_eegreadings = {};
+subj_ml = [];
+subj_pics = [];
 
 for curr_subject = SUBJECTS
     % load each subjects data for cleaning
@@ -20,13 +22,13 @@ for curr_subject = SUBJECTS
     
     % First, remove data around "boundary" events, as the data there is
     % corrupted
-    curr_EEG = pop_rmdat(EEG, BOUNDARY_EVENTS, [-0.5, 0.1], 1);
+    curr_EEG = pop_rmdat(curr_EEG, BOUNDARY_EVENTS, [-0.5, 0.1], 1);
     
     if SAVE_INTERMEDIATE
         pop_saveset(curr_EEG, 'filename', 'no_boundaries.set', 'filepath', FILEPATH);
     end
     
-    % Now break into two sets, ML and picture, and apply appropriate filter
+    % Now break into two sets, ML and picture, and apply appropriate filter    
     ml_eeg = pop_eegfiltnew(curr_EEG, 1, 50);
     pic_eeg = pop_eegfiltnew(curr_EEG, 3, 30);
     
@@ -48,16 +50,20 @@ for curr_subject = SUBJECTS
     ml_eeg = pop_rmbase(ml_eeg, [-200, 0]);
     pic_eeg = pop_rmbase(pic_eeg, [-200, 0]);
     
-    % do avg referencing
+    % do avg referencing -- TODO : look into if you're sure this is working
     ml_eeg = pop_reref(ml_eeg, []);
     pic_eeg = pop_reref(pic_eeg, []);
+     
+    pop_saveset(ml_eeg, 'filename', char(append(curr_subject, '_cleaned_ml.set')), 'filepath', FILEPATH);
+    pop_saveset(pic_eeg, 'filename', char(append(curr_subject, '_cleaned_pic.set')), 'filepath', FILEPATH);
     
-    % remove all data other than the important points
-    pic_eeg = pop_rmdat(pic_eeg, PIC_EVENTS, [-0.2, 1], 0);
-    ml_eeg = pop_rmdat(ml_eeg, ML_EVENTS, [-0.2, 1], 0);
-    
-    ml_filename = char(append(curr_subject, '_cleaned_ml.set'));
-    pic_filename = char(append(curr_subject, '_cleaned_pic.set'));    
-    pop_saveset(ml_eeg, 'filename', ml_filename, 'filepath', FILEPATH);
-    pop_saveset(pic_eeg, 'filename', pic_filename, 'filepath', FILEPATH);
+    pop_export(ml_eeg, char(append(FILEPATH_OUT, curr_subject, '_cleaned_ml.csv')), 'transpose', 'on', 'separator', ',');
+    pop_export(pic_eeg, char(append(FILEPATH_OUT, curr_subject, '_cleaned_pic.csv')), 'transpose', 'on', 'separator', ',');
 end
+
+
+
+
+
+
+
