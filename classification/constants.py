3c83445dfe7e4d48c2b63254a2fd396724fbeae2
karlslakov <karlslakov@gmail.com>
2019-11-26 10:36:34 -0800

    add averaging parameter, update wordlist

--- classification/constants.py
+++ classification/constants.py
@@ -1,5 +1,6 @@
-word_list = ["baby", "bear", "bird", "bunny", "cat", "dog", "duck", "mommy", "banana", "bottle", "cookie", "cracker", "cup", "juice", "milk", "spoon"]
-
+# word_list = ["baby", "bear", "bird", "bunny", "cat", "dog", "duck", "mommy", "banana", "bottle", "cookie", "cracker", "cup", "juice", "milk", "spoon"]
+word_list = ["baby", "BAD_STRING", "bird", "BAD_STRING", "cat", "dog", "duck", "mommy", "banana", "bottle", "cookie", "cracker", "BAD_STRING", "juice", "milk", "BAD_STRING"]
+# word_list = ["baby", "bird", "cat", "dog", "duck", "mommy", "banana", "bottle", "cookie", "cracker", "juice", "milk"]
 
 cleaned_data_filepath = "/Volumes/OFFCAMPUS/Jenn/Imported data/cleaned/"
 bad_trials_filepath = "/Volumes/OFFCAMPUS/Jenn/Datatracker/ML_badtrials-Table 1.csv"
