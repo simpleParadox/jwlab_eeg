d165dc78e7e07f259daf0c9f208e18fb49549249
Karl Slakov <kjslakov@gra-login2.graham.sharcnet>
2020-02-03 17:11:14 -0500

    participants

---
+++ classification/code/jwlab/run/train_eval_svm_participants.py
@@ -0,0 +1,31 @@
+print("start imports", flush=True)
+
+import run_setup
+
+import pandas as pd
+import numpy as np
+
+from jwlab.ml_prep import load_ml_df, y_to_binary, no_average, average_trials
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from jwlab.eval import eval_across_categories
+import sys
+
+print("finish imports, start loading df", flush=True)
+
+df = load_ml_df(sys.argv[1])
+print("loaded_df", flush=True)
+
+X,y,w,p = no_average(df)
+print("prepped train_data", flush=True)
+
+y = y_to_binary(y)
+
+model = LinearSVC()
+print("model created", flush=True)
+
+errs = eval_across_categories(model, X, y, p, 10, random_state=50)
+print("\n---\n")
+for j in range(errs.shape[0]):
+  print(np.mean(errs[j, :]))
---
+++ classification/jobs/train_eval_svm_participants.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --account=def-jwerker
+#SBATCH --mem=8G
+#SBATCH --time=0:40:00
+#SBATCH --output=output/%x-%j.out
+
+module load python/3.6
+module load scipy-stack
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install sklearn
+pip install sktime
+pip install --no-index -r general_requirements.txt
+mkdir $SLURM_TMPDIR/data
+echo "copying dataset"
+cp /$HOME/projects/def-jwerker/kjslakov/data/ml_df_readys.pkl $SLURM_TMPDIR/data/dataset.pkl
+echo "dataset copied"
+python /$HOME/projects/def-jwerker/kjslakov/jwlab_eeg/classification/code/jwlab/run/train_eval_svm_participants.py $SLURM_TMPDIR/data/dataset.pkl
