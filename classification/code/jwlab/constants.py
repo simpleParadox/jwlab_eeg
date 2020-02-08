c515297c420fd1c038c4c489c34372549260ceb0
RoxyKang <roxykangjz@hotmail.com>
2020-02-08 15:18:36 -0800

    update

--- classification/code/jwlab/constants.py
+++ classification/code/jwlab/constants.py
@@ -1,7 +1,11 @@
 # word_list = ["baby", "bear", "bird", "bunny", "cat", "dog", "duck", "mommy", "banana", "bottle", "cookie", "cracker", "cup", "juice", "milk", "spoon"]
 word_list = ["baby", "BAD_STRING", "bird", "BAD_STRING", "cat", "dog", "duck", "mommy", "banana", "bottle", "cookie", "cracker", "BAD_STRING", "juice", "milk", "BAD_STRING"]
+# word_list = ["baby", "bird", "cat", "dog", "duck", "mommy", "banana", "bottle", "cookie", "cracker", "juice", "milk"]
 
+#cleaned_data_filepath = "/Volumes/OFFCAMPUS/Jenn/Imported data/cleaned/"
+#bad_trials_filepath = "/Volumes/OFFCAMPUS/Jenn/Datatracker/ML_badtrials-Table 1.csv"
 
+<<<<<<< Updated upstream
 #cleaned_data_filepath = "/Volumes/OFFCAMPUS/Jenn/Imported data/cleaned/"
 #bad_trials_filepath = "/Volumes/OFFCAMPUS/Jenn/Datatracker/ML_badtrials-Table 2.csv"
 
@@ -9,3 +13,9 @@ cleaned_data_filepath = "/Users/roxyk/Desktop/lab/cleaned/"
 bad_trials_filepath = "/Users/roxyk/Desktop/lab/ML_badtrials-Table 1.csv"
 db_filepath = "/Users/roxyk/Desktop/lab/db/"
 messy_trials_filepath = "/Users/roxyk/Desktop/lab/Datatracker/ML-Table 1.csv"
+=======
+cleaned_data_filepath = "/Users/roxyk/Desktop/lab/cleaned/"
+bad_trials_filepath = "/Users/roxyk/Desktop/lab/ML_badtrials-Table 1.csv"
+db_filepath = "/Users/roxyk/Desktop/lab/db/"
+messy_trials_filepath = "/Users/roxyk/Desktop/lab/Datatracker/ML-Table 1.csv"
+>>>>>>> Stashed changes
