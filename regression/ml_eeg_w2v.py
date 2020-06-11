b2bc863627cbea8705f61cf2dc7b64551fac4fe1
Rohan <abomicode@gmail.com>
2020-06-11 01:57:11 -0600

    Monte-Carlo CV updated!

--- regression/ml_eeg_w2v.py
+++ regression/ml_eeg_w2v.py
@@ -49,9 +49,9 @@ elif os_name=='Linux':
     avg_trials_and_ps_9and13_path = os.getcwd() + "/regression/data/avg_trials_and_ps_9and13.pkl"
 
 # with open(pkl_path, 'rb') as f:
-# f = open(readys_path, 'rb')
-# readys_data = pickle.load(f)
-# f.close()
+f = open(readys_path, 'rb')
+readys_data = pickle.load(f)
+f.close()
 
 
 
@@ -65,13 +65,13 @@ if os_name =='Windows':
 elif os_name=='Linux':
     w2v_path = os.getcwd() + "/regression/w2v_embeds/all_w2v_embeds.npz"
     avg_w2v_path = os.getcwd() + "/regression/w2v_embeds/all_w2v_embeds_avg_trial.npz"
-    gen_w2v_all_ps_avg_path = "/regression/w2v_embeds/gen_w2v_embeds_avg_trial_and_ps.npz"
-# w2v_embeds_loaded = load(w2v_path)
-# w2v_embeds = w2v_embeds_loaded['arr_0']
-#
-# print("Data Loaded")
-# print("Readys shape: ", eeg_features.shape)
-# print("w2v shape: ", w2v_embeds.shape)
+    gen_w2v_all_ps_avg_path = os.getcwd() + "/regression/w2v_embeds/gen_w2v_embeds_avg_trial_and_ps.npz"
+w2v_embeds_loaded = load(w2v_path)
+w2v_embeds = w2v_embeds_loaded['arr_0']
+
+print("Data Loaded")
+print("Readys shape: ", readys_data.shape)
+print("w2v shape: ", w2v_embeds.shape)
 
 
   # The last value is the score.
@@ -107,7 +107,7 @@ def split_avg_trials_and_ps_model():
     print("Average score for 9 month averaged trials and ps: ", np.average(n_a_scores))
     print("Total time taken: ", stop - start)
 
-split_avg_trials_and_ps_model()
+# split_avg_trials_and_ps_model()
 
 def avg_trials_and_ps_model():
     # First read the data
@@ -226,14 +226,13 @@ def monte_carlo_2v2():
     print("Monte-Carlo CV")
     # Split into training and testing data
     parameters_ridge = {'alpha': [10000000, 100000000, 1000000000]} #0.01]}#, 0.1, 10, 20, 40, 80, 100, 1000, 10000, 100000, 1000000,
-    parameters_dt = {'random_state': [None]}
+    parameters_dt = {'min_samples_split': [2, 4, 6, 8, 10]}  #
 
-    ridge = DecisionTreeRegressor()
-    clf = GridSearchCV(ridge, param_grid=parameters_ridge, scoring='neg_mean_squared_error',
+    dt = DecisionTreeRegressor()
+    clf = GridSearchCV(dt, param_grid=parameters_dt, scoring='neg_mean_squared_error',
                        refit=True, cv=5, verbose=5)
-
-
-    rs = ShuffleSplit(n_splits=10, train_size=0.90)
+    eeg_features = readys_data.iloc[:, :18000].values
+    rs = ShuffleSplit(n_splits=8, train_size=0.90)
     all_data_indices = [i for i in range(len(w2v_embeds))]
     f = 1
     score_with_alpha = {}
@@ -242,16 +241,15 @@ def monte_carlo_2v2():
         print("Shuffle Split fold: ", f)
         X_train, X_test = eeg_features[train_index], eeg_features[test_index]
         # The following two lines are for the permutation test. Comment them out when not using the permutation test.
-        random.shuffle(train_index) # For permutation test only.
-        random.shuffle(test_index) # For permutation test only.
+        # random.shuffle(train_index) # For permutation test only.
+        # random.shuffle(test_index) # For permutation test only.
         y_train, y_test = w2v_embeds[train_index], w2v_embeds[test_index]
 
-        ss = StandardScaler()
-        X_train = ss.fit_transform(X_train)
-        X_test = ss.transform(X_test)
-
-        ridge.fit(X_train, y_train)
-        preds = ridge.predict(X_test)
+        # ss = StandardScaler()
+        # X_train = ss.fit_transform(X_train)
+        # X_test = ss.transform(X_test)
+        clf.fit(X_train, y_train)
+        preds = clf.predict(X_test)
         # print("Preds", preds.shape)
         # print("y_test:", y_test.shape)
         f += 1
@@ -266,7 +264,7 @@ def monte_carlo_2v2():
     stop = time.time()
     print("Total time: ", stop - start)
 
-# monte_carlo_2v2()
+monte_carlo_2v2()
 
 # split_ps_model()
 
