3754be0b4dda6d324d78d236902bc1ee133b4720
jenncscampbell <jenncscampbell@gmail.com>
2020-08-08 06:33:30 -0600

    all working now

--- classification/code/jwlab/cluster_analysis_perm.py
+++ classification/code/jwlab/cluster_analysis_perm.py
@@ -11,19 +11,59 @@ from matplotlib import pyplot as plt
 ################################ Analysis procedure ################################
 
 def cluster_analysis_procedure(age_group, useRandomizedLabel, averaging, sliding_window_config, cross_val_config):
-    num_folds, num_iterations = cross_val_config[0], cross_val_config[1]
+    num_folds, cross_val_iterations, sampling_iterations = cross_val_config[0], cross_val_config[1], cross_val_config[2]
+    
+    results = {}
 
-    X, y, good_trial_count, num_win = prep_ml(age_group, useRandomizedLabel, averaging, sliding_window_config, downsample_num=1000)
+    for i in range(sampling_iterations):
 
-    results = cross_validaton(num_iterations, num_win, num_folds, X, y)
+        X, y, good_trial_count, num_win = prep_ml(age_group, useRandomizedLabel, averaging, sliding_window_config, downsample_num=1000)
 
-    pvalues, tvalues = t_test(results, num_iterations, num_win, num_folds)
+        temp_results = cross_validaton(cross_val_iterations, num_win, num_folds, X, y)
+
+        for i in range(len(temp_results)):
+            if i not in results.keys():
+                results[i] = {}
+            for j in range(len(temp_results[i])):
+                if j in results[i].keys(): 
+                                    results[i][j] += temp_results[i][j]
+                else:
+                    results[i][j]= temp_results[i][j]
+                    
+      
+
+    pvalues, tvalues = t_test(results, num_win, num_folds)
 
     clusters = find_clusters(pvalues, tvalues)
 
     max_t_mass = get_max_t_mass(clusters, tvalues)
+    
+    ## REMOVE FOR NULL FUNCTION
+    if len(sliding_window_config[2]) == 1:
+        createGraph(results)
+    else: 
+        print("Graph function is not supported for multiple window sizes")
 
-    return max_t_mass
+    return results
+
+def createGraph(results):
+    scoreMean = []
+    stdev = []
+    for i in range(len(results)):
+        for j in range(len(results[i])):
+
+            scoreMean.append(round(np.mean(results[i][j]), 4))
+            stdev.append(round(stats.sem(results[i][j]), 4))
+
+
+    length_per_window_plt = 1200/ len(scoreMean)
+    x_graph = np.arange(-200,1000,length_per_window_plt) 
+    y_graph = scoreMean
+    stdevplt = np.array(stdev)
+    error = stdevplt
+    plt.plot(x_graph, y_graph, 'k-')
+    plt.fill_between(x_graph, y_graph-error, y_graph+error)
+    plt.show()
 
 def cross_validaton(num_iterations, num_win, num_folds, X, y):
     results = []
@@ -49,8 +89,7 @@ def cross_validaton(num_iterations, num_win, num_folds, X, y):
                 y_pred = model.predict(X_test)
                 testScore = accuracy_score(y_test,y_pred) 
                 
-                
-
+               
                 if j in temp_results.keys(): 
                     temp_results[j] += [testScore]
                 else:
@@ -67,21 +106,10 @@ def cross_validaton(num_iterations, num_win, num_folds, X, y):
         for j in range(num_win[i]):
             assert len(results[i][j]) == num_iterations * num_folds
         
-    
-    #supress graph for null dist
-    for i in range(len(scoreMean)):
-        length_per_window_plt = 1200/ len(scoreMean[i])
-        x_graph = np.arange(-200,1000,length_per_window_plt) 
-        y_graph = scoreMean[i]
-        stdevplt = np.array(stdev[i])
-        error = stdevplt
-        plt.plot(x_graph, y_graph, 'k-')
-        plt.fill_between(x_graph, y_graph-error, y_graph+error)
-        plt.show()
-    
     return results
 
-def t_test(results, num_iterations, num_win, num_folds):
+
+def t_test(results, num_win, num_folds):
     pvalues = []
     tvalues = []
     for i in range(len(results)):
@@ -89,14 +117,16 @@ def t_test(results, num_iterations, num_win, num_folds):
             # change the second argument below for comparison
             istat = stats.ttest_1samp(results[i][j], .5)
             pvalues += [istat.pvalue] if istat.statistic > 0 else [1]
+            # removed just so that we can get the negative value from the pre window
             tvalues += [istat.statistic] if istat.statistic > 0 else [0]
     
     return pvalues, tvalues
 
 # Finding contiguous time cluster
 def find_clusters(pvalues, tvalues):
+    #change to two tailed
     valid_window = [i for i,v in enumerate(pvalues) if v <= 0.025]
-    
+     ## REMOVE FOR NULL FUNCTION
     print("Valid windows are: {0}\n".format(valid_window))
     
     # Obtain clusters (3 or more consecutive meaningful time)
@@ -107,6 +137,8 @@ def find_clusters(pvalues, tvalues):
     for c in clusters: 
         new_list = [((x*10)-200) for x in c]
         adj_clusters.append(new_list)
+         
+    ## REMOVE FOR NULL FUNCTION
     print("Clusters are: {0}\n".format(adj_clusters))
     return clusters
 
@@ -120,6 +152,7 @@ def get_max_t_mass(clusters, tvalues):
     
     max_t_mass = max(t_mass)
     
+     ## REMOVE FOR NULL FUNCTION
     print("The max t mass is: {0}\n".format(max_t_mass))
     
     return max_t_mass
